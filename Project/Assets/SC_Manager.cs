13aaeefd1fedb0847bf13fea2659a564c82e9721
Daryl Hornsby <digitaldarylh@gmail.com>
2014-01-25 21:55:45 +0100

    Folders

---
+++ Project/Assets/SC_Manager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SC_Manager : MonoBehaviour {
+	
+	public GUISkin inGameGUI;
+	public int optionAmount;
+	public List<bool> option = new List<bool>();
+
+	int guiX = 10;
+	int guiY = 300;
+	int heightMult = 30;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		for(int i = 0; i < optionAmount; i++){
+			option[i] = GUI.Toggle (new Rect (guiX, guiY + (i*heightMult), 60, 15), option[i], "Testing Toggle Button");
+		}
+		if (GUI.Button (new Rect (0, 60, 150, 20), "Ok"))
+			print ("user clicked ok");
+	}
+}
---
+++ Project/Assets/Scripts/GM_Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Cheat
+		if (Input.GetKeyDown (KeyCode.P)) {
+			Application.LoadLevel(Application.loadedLevel +1);
+		}
+	}
+
+	public void SceneLoad(string l){
+		Application.LoadLevel (l);
+	}
+}
